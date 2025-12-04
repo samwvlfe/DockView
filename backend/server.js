72d87cab41fd34962ac54dbe18c59adcf1175088
samwvlfe <r.sam.wolfe@gmail.com>
2025-12-04 13:35:04 -0500

    first DB endpoint

--- backend/server.js
+++ backend/server.js
@@ -18,3 +18,18 @@ fastify.listen({ port: PORT, host: "0.0.0.0" }, (err) => {
   if (err) throw err;
   console.log("My own server running on port", PORT);
 });
+
+// ------------ READ ALL MESSAGES ------------
+fastify.get("/messages", async () => {
+  const { data, error } = await supabase
+    .from("messages")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return { error: "Failed to fetch messages" };
+  }
+
+  return { messages: data };
+});
