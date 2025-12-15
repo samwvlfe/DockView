9434ddc3122a4ae80ec88a16ad5f2e6a86f6f06f
samwvlfe <r.sam.wolfe@gmail.com>
2025-12-15 14:19:16 -0500

    padding on info-cont border, wrapper around info-cont widgets so headers are above

--- frontend/components/DockInfoCard.tsx
+++ frontend/components/DockInfoCard.tsx
@@ -9,35 +9,39 @@ interface DockInfoCardProps {
 export default function DockInfoCard({ dock }: DockInfoCardProps){
     //get dock bay info
     return(
-        <div className="widget stack gap">
+        <div className="widget stack gap" style={{padding: "5px"}}>
             <div className={`${styles['info-card']} row gap`}>
-                <div className="widget-hdr">Dock Info</div>
-                <div className="nested-widget stack gap" style={{minWidth: "103px"}}>
-                    <div><span className="bold">Name: </span><span className="orgColor">{dock.name}</span></div>
-                    <div><span className="bold">Status: </span><span className="orgColor">{dock.status}</span></div>
-                    <div><span className="bold">ID: </span><span className="orgColor">{dock.friendly_id}</span></div>
+                <div className="stack">
+                    <div className="widget-hdr">Dock Info</div>
+                    <div className="nested-widget stack gap" style={{minWidth: "103px"}}>
+                        <div><span className="bold">Name: </span><span className="orgColor">{dock.name}</span></div>
+                        <div><span className="bold">Status: </span><span className="orgColor">{dock.status}</span></div>
+                        <div><span className="bold">ID: </span><span className="orgColor">{dock.friendly_id}</span></div>
+                    </div>
                 </div>
-                <div className="widget-hdr">History</div>
-                <div className="nested-widget stack gap" style={{flex: "1"}}>
-                    <div className={`${styles.historyTable} stack`}>
-                        <div className={styles.historyHeader}>
-                            <span>Update</span>
-                            <span>Reason</span>
-                            <span>Time</span>
-                        </div>
-                        <div className={`${styles.historyRowCont} stack`}>
-                            {[...dock.history]
-                            .sort((a, b) =>
-                                new Date(b.created_at).getTime() -
-                                new Date(a.created_at).getTime()
-                            )
-                            .map((row) => (
-                                <div key={row.id} className={styles.historyRow}>
-                                    <span>{row.new_status}</span>
-                                    <span>{row.reason}</span>
-                                    <span>{formatTimeDate(row.created_at)}</span>
-                                </div>
-                            ))}
+                <div className="stack">
+                    <div className="widget-hdr">History</div>
+                    <div className="nested-widget stack gap" style={{flex: "1"}}>
+                        <div className={`${styles.historyTable} stack`}>
+                            <div className={styles.historyHeader}>
+                                <span>Update</span>
+                                <span>Reason</span>
+                                <span>Time</span>
+                            </div>
+                            <div className={`${styles.historyRowCont} stack`}>
+                                {[...dock.history]
+                                .sort((a, b) =>
+                                    new Date(b.created_at).getTime() -
+                                    new Date(a.created_at).getTime()
+                                )
+                                .map((row) => (
+                                    <div key={row.id} className={styles.historyRow}>
+                                        <span>{row.new_status}</span>
+                                        <span>{row.reason}</span>
+                                        <span>{formatTimeDate(row.created_at)}</span>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
