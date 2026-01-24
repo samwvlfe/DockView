9b3dc27f8f14ed70d09119398aa6115f69a90fbf
samwvlfe <r.sam.wolfe@gmail.com>
2026-01-24 15:36:05 -0500

    register new sensor route

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ fastify.register(require("@fastify/websocket"));
 
 // Register routes
 fastify.register(require("./routes/ws"));
-fastify.register(require("./routes/sensor"));
+fastify.register(require("./routes/sensors"));
+fastify.register(require("./routes/sensors/all"));
 fastify.register(require("./routes/docks"));
 fastify.register(require("./routes/index"));
 fastify.register(require("./routes/messages"));
--- frontend/app/(protected)/dashboard/page.tsx
+++ frontend/app/(protected)/dashboard/page.tsx
@@ -17,40 +17,39 @@ export default function DashboardPage() {
 
     // Fetch dock data first then listen for WS updates
     useEffect(() => {
-    const initializeData = async () => {
-        // Load initial docks and sensors
-        const initDocks = await fetchDocks();
-        setDocks(initDocks);
-        const initSensors = await fetchSensors();
-        setSensors(initSensors);
-        console.log(sensors);
+        const initializeData = async () => {
+            // Load initial docks and sensors
+            const initDocks = await fetchDocks();
+            setDocks(initDocks);
+            const initSensors = await fetchSensors();
+            setSensors(initSensors);
+            console.log("sensors: ", initSensors);
 
 
-        // Open WS connection
-        const ws = new WebSocket("wss://dockview.onrender.com/ws");
-        ws.onopen = () => console.log("WebSocket connection established");
-        // Handle incoming messages
-        ws.onmessage = (event) => {
-        const msg = JSON.parse(event.data);
-        // Dock status updates
-        if (msg.type === "dock_status_update") {
-            const payload = msg.payload;
-            // Update the relevant dock's status
-            setDocks((prevDocks) =>
-            prevDocks.map((dock: DockBay) =>
-                dock.id === payload.dock_bay_id
-                ? {
-                    ...dock,
-                    status: payload.new_status,
-                    status_changed_at: payload.status_changed_at
-                    }
-                : dock
-            )
-            );
-        }
+            // Open WS connection
+            const ws = new WebSocket("wss://dockview.onrender.com/ws");
+            ws.onopen = () => console.log("WebSocket connection established");
+            // Handle incoming messages
+            ws.onmessage = (event) => {
+                const msg = JSON.parse(event.data);
+                // Dock status updates
+                if (msg.type === "dock_status_update") {
+                    const payload = msg.payload;
+                    // Update the relevant dock's status
+                    setDocks((prevDocks) =>
+                    prevDocks.map((dock: DockBay) =>
+                        dock.id === payload.dock_bay_id
+                        ? {
+                            ...dock,
+                            status: payload.new_status,
+                            status_changed_at: payload.status_changed_at
+                            }
+                        : dock
+                    )
+                    );
+                }
+            };
         };
-    };
-
     initializeData();
   }, []);
 
