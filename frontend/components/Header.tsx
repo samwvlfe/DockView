3d4db25af5275a58a4d19f6227a2a88fcdd16865
samwvlfe <r.sam.wolfe@gmail.com>
2026-01-26 13:49:44 -0500

    add home button to header

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -4,18 +4,22 @@ import Image from "next/image";
 import dockstarLogo from "@/public/dockstar-xmas-logo.png";
 
 import Link from "next/link";
+import { DockTurnoverPayload } from '../types/websocketTypes';
 
 export default function Header() {
   return (
     <div className={`${styles.menu} apart`}>
-        <Image
-            className={styles.logo}
-            src={dockstarLogo}
-            alt="dockstar xmas logo"
-            width={7314}
-            height={2568}
-            priority
-        />
+        <div className="row center">
+          <Image
+              className={styles.logo}
+              src={dockstarLogo}
+              alt="dockstar xmas logo"
+              width={7314}
+              height={2568}
+              priority
+          />
+          <div className={styles.option}><Link href="/dashboard" target="_blank">Home</Link></div>
+        </div>
         <div className={styles["option-wrapper"]}>
             <div className={styles.option}><Link href="/controller" target="_blank">Controller</Link></div>
             {/* <div className={styles.option}>Home</div>
