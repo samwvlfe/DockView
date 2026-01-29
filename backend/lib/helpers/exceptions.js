d6b0b2a7b395d9a0bfd0e835d62e6ea70f16e458
samwvlfe <r.sam.wolfe@gmail.com>
2026-01-29 13:09:16 -0500

    add new exception case

--- backend/lib/helpers/exceptions.js
+++ backend/lib/helpers/exceptions.js
@@ -1,27 +1,35 @@
 // exceptionHandler.js
 
 function getExceptionPayload(lastState, sensor) {
-  console.log("sensor: ", sensor);
-  console.log("lastState: ", lastState);
   // Door opened before restraint
-  if (lastState === "Truck_Present" && sensor.sensor_type === "DOOR") {
-    return {
-      exception_code: "Invalid_Open",
-      message: "Tried To Open Door Before Restraint In Place",
-      sensor: sensor,  // Pass the entire sensor object
-      fix: "Close Door",
-    };
+  if (lastState === "Truck_Present"){
+    if(sensor.sensor_type === "DOOR") {
+        return {
+            exception_code: "Invalid_Open",
+            message: "Tried To Open Door Before Restraint In Place",
+            sensor: sensor,  // Pass the entire sensor object
+            fix: "Close Door, Restrain Truck",
+        };
+    }
+    if(sensor.sensor_type === "LEVELER") {
+        return {
+            exception_code: "Invalid_Open",
+            message: "Tried To Open Leveler Before Restraint In Place",
+            sensor: sensor,  // Pass the entire sensor object
+            fix: "Close Leveler, Restrain Truck",
+        };
+    }
   }
-  
-  // Leveler opened before restraint
-  if (lastState === "Truck_Present" && sensor.type === "LEVELER") {
-    return {
-      exception_code: "Invalid_Open",
-      message: "Tried To Open Leveler Before Restraint In Place",
-      sensor: sensor,  // Pass the entire sensor object
-      fix: "Close Leveler",
-    };
-  }
+  else if(lastState === "Restrained_DoorClosed"){
+    if(sensor.sensor_type === "LEVELER"){
+        return {
+            exception_code: "Invalid_Open",
+            message: "Tried To Open Leveler Before Door Open",
+            sensor: sensor,  // Pass the entire sensor object
+            fix: "Close Leveler, Open Door",
+        };
+    }
+  } 
   
   // No exception detected
   return null;
